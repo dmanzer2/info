49bf7d09785c4077c72ee5b251625784446058f5
David Manzer <David.Manzer@worldatwork.org>
2016-02-22 11:46:04 -0700

    made a fix

--- js/main.js
+++ js/main.js
@@ -203,10 +203,9 @@ MANZER.contactForm = function(){
 			success: function(data) {
 				$contactForm.find('.alert-standard').fadeOut(1500);
 				$contactForm.append('<div class="alert alert-success">Message sent!</div>');
-				$contactForm.find('.alert-success').fadeOut(6500);
 			},
 			error: function(err) {
-				$contactForm.find('.alert-standard').fadeOut(3500);
+				$contactForm.find('.alert-standard').hide();
 				$contactForm.append('<div class="alert alert-error">Ops, there was an error.</div>');
 			}
 		});
