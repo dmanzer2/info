5a589cc6a6ee00f4eb218701030dd6222550a6fc
David Manzer <David.Manzer@worldatwork.org>
2016-02-22 11:09:44 -0700

    alert fix

--- js/main.js
+++ js/main.js
@@ -199,15 +199,15 @@ MANZER.contactForm = function(){
 			data: $(this).serialize(),
 			dataType: 'json',
 			beforeSend: function() {
-				$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+				$contactForm.append('<div class="alert alert-standard">Sending message…</div>');
 			},
 			success: function(data) {
 				$contactForm.find('.alert--loading').hide();
-				$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+				$contactForm.append('<div class="alert alert-success">Message sent!</div>');
 			},
 			error: function(err) {
 				$contactForm.find('.alert--loading').hide();
-				$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+				$contactForm.append('<div class="alert alert-error">Ops, there was an error.</div>');
 			}
 		});
 	});
