26a45bdfafb9c5d7468cc71b37a3e406906b50e3
David Manzer <David.Manzer@worldatwork.org>
2016-02-22 15:23:19 -0700

    test

--- js/main.js
+++ js/main.js
@@ -203,6 +203,7 @@ MANZER.contactForm = function(){
 			success: function(data) {
 				$contactForm.find('.alert-standard').fadeOut(1500);
 				$contactForm.append('<div class="alert alert-success">Message sent!</div>');
+				$contactForm.find('.alert-success').slideDown(1500);
 			},
 			error: function(err) {
 				$contactForm.find('.alert-standard').hide();
