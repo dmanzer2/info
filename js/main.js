47564b3e66deb6b627f86bdb50ea1a881da2e393
David Manzer <David.Manzer@worldatwork.org>
2016-02-23 12:52:16 -0700

    this should work

--- js/main.js
+++ js/main.js
@@ -203,8 +203,7 @@ MANZER.contactForm = function(){
 			success: function(data) {
 				$contactForm.find('.alert-standard').fadeOut(1500);
 				$contactForm.append('<div class="alert alert-success">Message sent!</div>');
-				$contactForm.delay(6500).reset();
-
+				$contactForm[0].reset();
 			},
 			error: function(err) {
 				$contactForm.find('.alert-standard').hide();
