b94353752fce214a4faf587b50fe06c20669d864
David Manzer <David.Manzer@worldatwork.org>
2016-02-23 15:01:17 -0700

    working with angular

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,72 @@
-var dmanzerApp = angular.module('dmanzerApp',[]);
+var myApp = angular.module('myApp',[]);
 
 dmanzerApp.controller('WorkController', ['$scope', function($scope) {
-  $scope.web = {
-    'name' : 'item01',
-    'medium' : 'web development',
-    'client' : 'new',
+  $scope.webdevelopment = [
+    {
+    "name" : "image-web-01",
+    "shortname" : "image-web-01",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-02",
+    "shortname" : "image-web-02",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-03",
+    "shortname" : "image-web-03",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-04",
+    "shortname" : "image-web-04",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-05",
+    "shortname" : "image-web-05",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-06",
+    "shortname" : "image-web-06",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-07",
+    "shortname" : "image-web-07",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-08",
+    "shortname" : "image-web-08",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
+    {
+    "name" : "image-web-09",
+    "shortname" : "image-web-09",
+    "medium" : "web development",
+    "client" : "employer",
+    "size" : "full"
+    },
 
-  }
+
+]
 
 }]);
