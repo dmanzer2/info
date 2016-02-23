494ff7f2ae3c0f64e9569f3a198583b255ccc6d9
David Manzer <David.Manzer@worldatwork.org>
2016-02-23 12:47:09 -0700

    okay now this

--- js/main.js
+++ js/main.js
@@ -204,7 +204,7 @@ MANZER.contactForm = function(){
 				$contactForm.find('.alert-standard').fadeOut(1500);
 				$contactForm.append('<div class="alert alert-success">Message sent!</div>');
 				$contactForm.delay('<div class="alert alert-success">Message sent!</div>', 6500);
-				$contactForm("form").trigger("reset");
+				this.reset();
 			},
 			error: function(err) {
 				$contactForm.find('.alert-standard').hide();
