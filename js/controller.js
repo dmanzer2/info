f6215552806993010b511f96b825fff950505cfc
David Manzer <David.Manzer@worldatwork.org>
2016-02-24 08:33:26 -0700

    minor controller edit

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('WorkController', function WorkController($scope, $http) {
+myApp.controller('WorkController', ['$scope', '$http', function ($scope, $http) {
   $http.get('js/lay-des.json').success(function(data) {
     $scope.layout = data;
   });
-});
+}]);
