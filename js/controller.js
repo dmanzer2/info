e6439f47ec50333eb4ea88e10d03ebc3685a6bc9
David Manzer <David.Manzer@worldatwork.org>
2016-02-24 07:44:09 -0700

    angular work and minor edits

--- js/controller.js
+++ js/controller.js
@@ -1,69 +1,69 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('WorkController', ['$scope', function($scope) {
+myApp.controller('WorkController', function WorkController($scope) {
   $scope.webdevelopment = [
     {
     "name" : "image-web-01",
     "shortname" : "image-web-01",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-02",
     "shortname" : "image-web-02",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-03",
     "shortname" : "image-web-03",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-04",
     "shortname" : "image-web-04",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-05",
     "shortname" : "image-web-05",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-06",
     "shortname" : "image-web-06",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-07",
     "shortname" : "image-web-07",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-08",
     "shortname" : "image-web-08",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
     {
     "name" : "image-web-09",
     "shortname" : "image-web-09",
     "medium" : "web development",
     "client" : "employer",
-    "size" : "full"
+    "description" : "Here's where it goes"
     },
   ],
   $scope.layout = [
