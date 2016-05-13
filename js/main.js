6388d657b5ac8acbd1561b5c88ba3bd793fc36a3
David Manzer <David.Manzer@worldatwork.org>
2016-05-13 10:33:08 -0700

    minor tweak

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ MANZER.slider = function(){
 											},
 											{
 												image : 'img/slider-images/image02.jpg',
-												title : '<div class="slide-content">Tools I used for this site.</div><div class="slide-content-title" style="text-transform: lowercase; letter-spacing: 1px;">HTML5, CSS3, Javascript, jQuery, Angular.JS<br>JSON, Ajax, node.js, express, Grunt.js, bower.js, github</div>',
+												title : '<div class="slide-content">Tools I used<br>for this site.</div><div class="slide-content-title" style="text-transform: lowercase; letter-spacing: 1px;">HTML5, CSS3, Javascript, jQuery, Angular.JS<br>JSON, Ajax, node.js, express,<br>Grunt.js, bower.js, github</div>',
 												thumb : '',
 												url : ''
 											},
@@ -96,8 +96,8 @@ MANZER.slider = function(){
 												url : ''
 											},
 											{
-												image : 'img/slider-images/image04.jpg',
-												title : '<div class="slide-content">Tools I used for this site.</div><div class="slide-content-title" style="text-transform: lowercase; letter-spacing: 1px;">HTML5, CSS3, Javascript, jQuery, Angular.JS<br>JSON, Ajax, node.js, express, Grunt.js, bower.js, github</div>',
+												image : 'img/slider-images/image02.jpg',
+												title : '<div class="slide-content">Tools I used<br>for this site.</div><div class="slide-content-title" style="text-transform: lowercase; letter-spacing: 1px;">HTML5, CSS3, Javascript, jQuery, Angular.JS<br>JSON, Ajax, node.js, express,<br>Grunt.js, bower.js, github</div>',
 												thumb : '',
 												url : ''
 											}
