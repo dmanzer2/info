dc39698f4f24eea591651fce27f6dce64a2fae04
David Manzer <david.manzer@kaseya.com>
2017-06-02 19:28:00 -0700

    minified css

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,12 +18,33 @@ module.exports = function(grunt){
 				}
 			}
 		},
+		cssmin: {
+		  minify: {
+		    files: [{
+		      expand: true,
+		      cwd: 'css',
+		      src: ['**/*.css', '!**/*.min.css'],
+		      dest: 'css',
+		      ext: '.min.css'
+		    }]
+		  },
+		  options: {
+		    shorthandCompacting: false,
+		    roundingPrecision: -1
+		  },
+		  combine: {
+		    files: {
+		      'css/style.css': ['css/bootstrap.min.css', 'css/main.css','css/supersized.css','css/supersized.shutter.css','css/fancybox/jquery.fancybox.css','css/fonts.css','css/font-awesome.css','css/bootstrap-responsive.min.css','css/responsive.css','css/supersized.css','css/supersized.shutter.css']
+		    }
+		  }
+		}
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-express');
-	grunt.registerTask('default',['express','watch']);
-	grunt.registerTask('serve',['express','watch']);
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default',['express','watch','cssmin']);
+	grunt.registerTask('serve',['express','watch','cssmin']);
 
 	};
