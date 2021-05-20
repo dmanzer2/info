1f0cf087b37bbb391e3c029196093ad7f7165fd8
David Manzer <dmanzer2@gmail.com>
2021-05-20 11:10:50 -0700

    fixed scroll nav

--- js/main.js
+++ js/main.js
@@ -482,3 +482,10 @@ $(window).resize(function(){
 });
 
 });
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
